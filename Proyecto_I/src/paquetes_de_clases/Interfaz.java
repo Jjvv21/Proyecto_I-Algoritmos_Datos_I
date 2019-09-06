763ddb77b78579daa2e2672bb3891b7f8d1c7ab3
Jjvv21 <47864787+Jjvv21@users.noreply.github.com>
2019-09-06 03:41:41 -0600

    Estructura casi Finalizada

--- Proyecto_I/src/paquetes_de_clases/Interfaz.java
+++ Proyecto_I/src/paquetes_de_clases/Interfaz.java
@@ -1,7 +1,10 @@
 package paquetes_de_clases;
 
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -10,6 +13,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
@@ -22,6 +27,7 @@ public class Interfaz extends Application {
      //   super.init();
       //  System.out.println("Realice las inicializaciones necesarias aquí.");
     //}
+	Button ayuda; 
 
     public void start(Stage primaryStage) throws Exception {
     	//COLUMNA VERTICAL 
@@ -52,7 +58,41 @@ public class Interfaz extends Application {
     	 * Esta es la estructura de la interfaz con respecto a la parte Derecha de la ventana 
     	 */
         VBox Caja_Derecha = new VBox(); 
+        FileInputStream AND_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\AND2.png"); 
+        Image AND_image = new Image(AND_img); 
+        ImageView A = new ImageView(AND_image); 
+        Label AND = new Label("", A); 
         
+        FileInputStream NAND_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\NAND.png"); 
+        Image NAND_image = new Image(NAND_img); 
+        ImageView B = new ImageView(NAND_image); 
+        Label NAND = new Label("", B); 
+        
+        FileInputStream NOR_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\NOR.png"); 
+        Image NOR_image = new Image(NOR_img); 
+        ImageView C = new ImageView(NOR_image); 
+        Label NOR = new Label("", C); 
+        
+        FileInputStream XNOR_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\XNOR.png"); 
+        Image XNOR_image = new Image(XNOR_img); 
+        ImageView D = new ImageView(XNOR_image); 
+        Label XNOR = new Label("", D); 
+        
+        FileInputStream NOT_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\NOT.png"); 
+        Image NOT_image = new Image(NOT_img); 
+        ImageView E = new ImageView(NOT_image); 
+        Label NOT = new Label("", E); 
+        
+        FileInputStream XOR_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\XOR.png"); 
+        Image XOR_image = new Image(XOR_img); 
+        ImageView F = new ImageView(XOR_image); 
+        Label XOR = new Label("", F); 
+        
+        FileInputStream OR_img = new FileInputStream("C:\\Users\\Julio\\git\\Proyecto_v2\\Proyecto_I\\Imagenes\\OR.png"); 
+        Image OR_image = new Image(OR_img); 
+        ImageView G = new ImageView(OR_image); 
+        Label OR = new Label("", G); 
+       
         /**
     	 * Esta es la estructura de la interfaz con respecto a la parte de arriba en la ventana 
     	 * Se utiliza una HBox y cinco Botones
@@ -66,7 +106,8 @@ public class Interfaz extends Application {
         agregar_circuito.setStyle("-fx-padding: 10 10 10 10;");
         Button herramientas = new Button("Herramientas");
         herramientas.setStyle("-fx-padding: 10 10 10 10;");
-        Button ayuda = new Button("Ayuda");
+        ayuda = new Button("Ayuda");
+        //ayuda.setOnAction(e-> ButtonClicked(e));
         ayuda.setStyle("-fx-padding: 10 10 10 10;");
         ayuda.setTranslateX(1340);
         /**
@@ -97,11 +138,20 @@ public class Interfaz extends Application {
     	 *un VBox que se desplaza junto con los 5 botones.  
     	 */
         ToolBar ToolBar_izquierda = new ToolBar();
-        Caja_izquierda.getChildren().addAll(simular,Limpiar,Nuevo_Proyecto,Borrar_un_elemento,Retroceder);
+        Caja_izquierda.getChildren().addAll(Limpiar,Nuevo_Proyecto,Borrar_un_elemento,Retroceder);
         ToolBar_izquierda.getItems().add(Caja_izquierda);
         ToolBar_izquierda.setOrientation(Orientation.VERTICAL);
         
-        
+
+        /**
+    	 *La parte "Derecha" de la ventana tiene un "ToolBar" con una orientacion vertical y las imagenes dentro de este panel se encuentran dentro de 
+    	 *un VBox que se desplaza junto con las 5 imagenes.  
+    	 */
+        ToolBar ToolBar_derecha = new ToolBar();
+        Caja_Derecha.getChildren().addAll(AND,NOT,XOR,XNOR,NOR,NAND,OR,simular);
+        Caja_Derecha.setSpacing(20);
+        ToolBar_derecha.getItems().add(Caja_Derecha);
+        ToolBar_derecha.setOrientation(Orientation.VERTICAL);
         
         
         
@@ -110,7 +160,7 @@ public class Interfaz extends Application {
         BorderPane.setMargin(Caja_izquierda, insets);
         Estructura.setLeft(ToolBar_izquierda);
         Estructura.setTop(ToolBar_superior);
-        Estructura.setRight(Caja_Derecha);
+        Estructura.setRight(ToolBar_derecha);
         
         
         
@@ -121,7 +171,8 @@ public class Interfaz extends Application {
         primaryStage.show();
     }
 
-    //public void stop() throws Exception {
+   
+	//public void stop() throws Exception {
        // super.stop();
         //System.out.println("Destruye los recursos. Realice limpieza.");
     //}
